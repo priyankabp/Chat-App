8161c453c967d16cf0c1f8b2ffd73bdfe8f53896
Priyanka Phapale <pbphapale@gmail.com>
2018-04-05 18:27:24 -0400

    Add web pack to bundle and build react code
    Add web pack to build project
    Combining controllers using custom html tags of controller names

---
+++ src/components/Zones.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Zones extends Component {
+    render(){
+        return (
+            <div>
+                Zones!!
+            </div>
+        )
+    }
+}
+
+export default Zones
